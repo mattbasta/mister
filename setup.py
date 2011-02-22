ab08b3b5d456c29cce0bcd99883aad5c322cd45f
Matt Basta <me@mattbasta.com>
2011-02-22 15:38:23 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    me='mister',
+    version='0.5',
+    description='A friendly, simple map/reduce library.',
+    long_description=open('README').read(),
+    author='Matt Basta',
+    author_email='me@mattbasta.com',
+    url='http://github.com/mattbasta/mister',
+    license='BSD',
+    packages=find_packages(exclude=['tests',
+                                    'tests/*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[p.strip() for p in open('./requirements.txt')
+                                              if not p.startswith('#')],
+    scripts=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',]
+    )
+
