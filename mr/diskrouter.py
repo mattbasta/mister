02eab3ad1532e83aef68071856f77e85fd70235f
Matt Basta <me@mattbasta.com>
2011-03-16 21:20:39 +0000

    Updated diskrouter to match default router.

--- mr/diskrouter.py
+++ mr/diskrouter.py
@@ -3,6 +3,7 @@ import cPickle
 from router import Router
 from constants import MAP, FILTER, REDUCE
 
+
 class DiskRouter(Router):
     "A baseline object that routes mister data within a single thread"
 
@@ -18,9 +19,12 @@ class DiskRouter(Router):
             if any(filter_(datum) for filter_ in self.hooks[FILTER][pattern]):
                 return
 
+        # Add the pattern if it doesn't exist
         if pattern not in self.data:
             self.data[pattern] = \
                 tempfile.SpooledTemporaryFile(max_size=self.spool_size)
+
+        # Pickle the datum and save it to the file buffer
         cPickle.dump(datum,
                      self.data[pattern],
                      protocol=cPickle.HIGHEST_PROTOCOL)
@@ -34,38 +38,66 @@ class DiskRouter(Router):
 
         spool.seek(0)
 
-        data = []
-        size = 0
         try:
+            # Unpickle until we get an end-of-file exception
             while True:
-                datum = cPickle.load(spool)
-                data.append(datum)
-                if spool.tell() > self.spool_size:
-                    yield data
-                    data = []
+                yield cPickle.load(spool)
         except EOFError:
             pass
 
+        # Close the temporary file when we're done
         spool.close()
-        if data:
-            yield data
 
     def process(self):
         "Processes the data and yields the result"
+
         while self.data:
+
+            skip_reduction = False
+
+            # Apply processing to mappers
+            for pattern in filter(lambda p: p in self.hooks[MAP] and
+                                            p not in self.hooks[REDUCE],
+                                  self.data.keys()):
+
+                data = self._gather_data(pattern)
+
+                # Mappers must be fed individual datums
+                mapper = self.hooks[MAP][pattern]
+                for datum in data:
+                    for new_pattern, new_datum in mapper(datum):
+                        skip_reduction = True
+                        self.feed(new_pattern, new_datum)
+
+            # Skip the reduction if we got new datums
+            if skip_reduction:
+                continue
+
+            # Flag to determine whether everything left is an orphan
+            all_orphans = True
+
+            # Apply processing for reducers and yieldables
             for pattern in self.data.keys():
-                data_generator = self._gather_data(pattern)
-                for data in data_generator:
-                    if pattern in self.hooks[REDUCE]:
-                        gen = self.hooks[REDUCE][pattern](data)
-                        if gen is None:
-                            continue
-                        for pattern, datum in gen:
-                            self.feed(pattern, datum)
-                    elif pattern in self.hooks[MAP]:
-                        for datum in data:
-                            for pattern, new_datum in self.hooks[MAP][pattern](datum):
-                                self.feed(pattern, new_datum)
-                    else:
-                        yield pattern, data
+
+                data = list(self._gather_data(pattern))
+
+                # If the pattern has no reducer, then yield it
+                if pattern not in self.hooks[REDUCE]:
+                    yield pattern, data
+                    continue
+
+                if len(data) > 1:
+                    gen = self.hooks[REDUCE][pattern](data)
+                    # Skip reducers that cancel out
+                    if gen is None:
+                        continue
+                    for new_pattern, datum in gen:
+                        all_orphans = False # We have more to process!
+                        self.feed(new_pattern, datum)
+                else:
+                    self.feed(pattern, data[0])
+
+            if all_orphans:
+                for pattern in self.data.keys():
+                    yield pattern, list(self._gather_data(pattern))
 
